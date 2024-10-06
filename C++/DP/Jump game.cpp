cc892565e7f8bb079a2109df5f5fe8d9dcfd6034
anuj2331 <anujm5207@gmail.com>
2024-10-06 13:27:58 +0000

    optimizecode2

--- C++/DP/Jump game.cpp
+++ C++/DP/Jump game.cpp
@@ -1,23 +1,31 @@
-#include<bits/stdc++.h>
+ #include <iostream>
+#include <vector>
 using namespace std;
-bool canJump(vector<int>&arr,int size){
-   int mxReach=0;
-   for(int i=0;i<size;i++){
-       if(i>mxReach){
-          return false;
-       }
-       mxReach=max(mxReach,i+arr[i]);
-   }
-  return true;
+
+bool canJump(const vector<int>& arr) {
+    int maxReach = 0;
+    int size = arr.size();
+    
+    for (int i = 0; i < size; i++) {
+        if (i > maxReach) {
+            return false;
+        }
+        maxReach = max(maxReach, i + arr[i]);
+    }
+    return true;
 }
-int main(){
-   int size;
-   cin>>size;
-   vector<int>arr; 
-   for(int i=0;i<size;i++){
-      cin>>arr[i];
-   }
-   bool ans=canJump(arr,size);
-   cout<<ans<<endl;
-   return 0;
+
+int main() {
+    int size;
+    cin >> size;
+    vector<int> arr(size); // Reserve space for size elements
+    
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i]; // Fill the vector with input values
+    }
+    
+    bool ans = canJump(arr);
+    cout << (ans ? "True" : "False") << endl; // Output "True" or "False"
+    
+    return 0;
 }
